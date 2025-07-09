8b72dea2c10c4f1e122c766c68ec71d103a64cb9
kelly-alphador <kellyalphador@gmail.com>
2025-07-09 22:25:29 +0300

    modifictaion dans program.cs et MesMethodeList

--- CSharp-Daily-5-LINQ-5-Algos/Program.cs
+++ CSharp-Daily-5-LINQ-5-Algos/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using MesMethodes.core;
+
+namespace Travail_algo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 7, 8, 9 };
+            MesMethodesList mesMethodesList = new MesMethodesList();
+            mesMethodesList.InverseListe(list);
+            int s = mesMethodesList.SommeListe(list);
+            Console.WriteLine(s);
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- MesMethodes.core/MesMethodesList.cs
+++ MesMethodes.core/MesMethodesList.cs
@@ -6,7 +6,103 @@ using System.Threading.Tasks;
 
 namespace MesMethodes.core
 {
-    internal class MesMethodesList
+    public class MesMethodesList
     {
+        public List<int> InverseListe(List<int> list)
+        {
+            int tmp;
+            for (int i = 0; i < list.Count / 2; i++)
+            {
+                tmp = list[i];
+                list[i] = list[list.Count - 1 - i];
+                list[list.Count - 1 - i] = tmp;
+            }
+            return list;
+        }
+        public int SommeListe(List<int> list)
+        {
+            int s = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                s = s + list[i];
+            }
+            return s;
+        }
+        public bool RechercherElement(List<int> list, int n)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == n)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public List<int> TrouverDoublons(List<int> list)
+        {
+            List<int> result = new List<int>();
+            int tmp;
+            bool test;
+            for (int i = 0; i < list.Count; i++)
+            {
+                tmp = list[i];
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (tmp == list[j])
+                    {
+                        test = false;
+                        for (int k = 0; k < result.Count; k++)
+                        {
+                            if (result[k] == tmp)
+                            {
+                                test = true;
+                                break;
+                            }
+                        }
+                        if (test == false)
+                        {
+                            result.Add(tmp);
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+        public List<int> FusionnerListes(List<int> list1, List<int> list2)
+        {
+            List<int> ListPasDoublons = new List<int>();
+            List<int> list = new List<int>();
+            bool test;
+            for (int i = 0; i < list1.Count; i++)
+            {
+                list.Add(list1[i]);
+            }
+            for (int i = 0; i < list2.Count; i++)
+            {
+                list.Add(list2[i]);
+            }
+            ListPasDoublons.Add(list[0]);
+            for (int i = 1; i < list.Count; i++)
+            {
+                test = false;
+                for (int k = 0; k < ListPasDoublons.Count; k++)
+                {
+
+                    if (list[i] == ListPasDoublons[k])
+                    {
+                        test = true;
+                        break;
+                    }
+
+                }
+                if (test == false)
+                {
+                    ListPasDoublons.Add(list[i]);
+                }
+
+            }
+            return ListPasDoublons;
+        }
     }
 }
